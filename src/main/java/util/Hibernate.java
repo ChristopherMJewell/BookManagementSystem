631bdf9cb7bc142acabc6665a1f778bf7b64a91e
Neeme <lneeme35@gmail.com>
2020-11-02 19:24:14 +0200

    Fixed Hibernate.java to create correct database (BookManagementSystem) and disabled line 37

--- src/main/java/util/Hibernate.java
+++ src/main/java/util/Hibernate.java
@@ -29,12 +29,12 @@ public class Hibernate {
             Properties properties = new Properties();
 
             properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-            properties.put(Environment.URL, "jdbc:mysql://localhost:3306/users?createDatabaseIfNotExist=true&serverTimeZone=UTC");
+            properties.put(Environment.URL, "jdbc:mysql://localhost:3306/BookManagementSystem?createDatabaseIfNotExist=true&serverTimeZone=UTC");
             properties.put(Environment.USER, "root");
-            properties.put(Environment.PASS, "Faithful777");
+            properties.put(Environment.PASS, "Permanent1!");
             properties.put(Environment.SHOW_SQL, "true");
             properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
-            properties.put(Environment.HBM2DDL_AUTO, "create-drop");
+//            properties.put(Environment.HBM2DDL_AUTO, "create-drop");
             properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 
             configuration.setProperties(properties);
